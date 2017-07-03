459acc7e5ec6a0d0aea6c63db355afceba791dcc
Sherwin Garcia-Obregon <sherwination@gmail.com>
2017-07-03 16:15:45 -0400

    Added Express App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { CommentsComponent } from './comments/comments.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ReviewsComponent,
+    ArticlesComponent,
+    CommentsComponent,
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
